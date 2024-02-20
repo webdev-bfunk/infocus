a668f45b6bbbed6129e8840b3c385982b082bdc3
webdev-bfunk <webdev.bfunk@gmail.com>
2024-02-19 19:00:00 -0500

    Initial commit from Create Next App

---
+++ data/nav.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+      label: "Home",
+      route: "/",
+      icon: "/home.svg",
+    },
+    {
+      label: "Services",
+      route: "/services",
+      icon: "/image.svg",
+    },
+    {
+      label: "Portfolio",
+      route: "/portfolio",
+      icon: "/stars.svg",
+    },
+    {
+      label: "Contact",
+      route: "/contact",
+      icon: "/scan.svg",
+    },
+]
